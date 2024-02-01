97c4e00590b0aaa1e7d1723cf3a84807b64a4a9d
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-02-01 06:51:48 -0600

    Updated environment variables and database connection

--- web/html/includes/database.php
+++ web/html/includes/database.php
@@ -4,5 +4,5 @@ $db = @mysqli_connect(
 	getenv("MYSQL_USER"),
 	getenv("MYSQL_PASSWORD"),
 	getenv("MYSQL_DATABASE"))
-		or die('Error connecting to database');
-		//or die('Error connecting to database: ' . mysqli_connect_error()); // for debugging
+		//or die('Error connecting to database');
+		or die('Error connecting to database: ' . mysqli_connect_error()); // for debugging
